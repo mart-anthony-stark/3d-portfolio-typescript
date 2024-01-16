20415db024c5e92e07c71b1877a67f6f8b1d8003
mart-anthony-stark <martsalazar0@gmail.com>
2024-01-16 09:14:41 +0800

    Fix: #1 remove inferred type annotation

--- src/components/CursorDiv.ts
+++ src/components/CursorDiv.ts
@@ -2,7 +2,7 @@ import { ComponentBuilder } from "../renderer";
 import "../styles/cursor.css";
 
 export class CursorDiv extends ComponentBuilder {
-  template: string = `
+  template = `
     <div id="cursor"></div>
     `;
 
